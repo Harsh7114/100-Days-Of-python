85c3344db93d251404f991d57d15eb83bb215b0f
Harsh7114 <harshranjan2582779@gmail.com>
2026-08-11 02:53:33 +0530

    day 22 complete ✅

--- DAY22_ping_pong/ball.py
+++ DAY22_ping_pong/ball.py
@@ -6,10 +6,25 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move= 10
+        self.y_move= 10
+        self.speed("fastest")
+        self.ball_move_speed = 0.1
     def move(self):
-        new_x = self.xcor()+10
-        new_y = self.ycor()+10
+        new_x = self.xcor()+self.x_move
+        new_y = self.ycor()+self.y_move
         self.goto(new_x,new_y)
 
-    def collision(self):
+    def bounce_y(self):
+        self.y_move *= -1
+    # def collision(self):
 
+    def bounce_x(self):
+        self.x_move *= -1
+        self.ball_move_speed *=0.9
+
+    def center(self):
+        self.goto(0, 0)
+        self.ball_move_speed =0.1
+        self.bounce_x()
+        #self.x_move = direction
--- DAY22_ping_pong/main.py
+++ DAY22_ping_pong/main.py
@@ -1,11 +1,13 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import  Scoreboard
 import time
-screen= Screen()
+screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800,height=600)
 screen.title("PING PONG GAME")
+screen.tracer(0)
 
 screen.listen()
 
@@ -15,6 +17,8 @@ screen.listen()
 left_paddle = Paddle((-350,0))
 right_paddle = Paddle((350,0))
 ball = Ball()
+scoreboard = Scoreboard()
+
 screen.onkey(right_paddle.move_up, "Up")
 screen.onkey(right_paddle.move_down, "Down")
 
@@ -23,9 +27,32 @@ screen.onkey(left_paddle.move_down, "s")
 
 game_on = True
 while game_on:
-    time.sleep(0.1)
+    time.sleep(ball.ball_move_speed)
     screen.update()
     ball.move()
 
-screen.exitonclick()
+    #detect collision with upper and lower wall
+    if ball.ycor() > 280 or ball.ycor()<-280:
+        #to bounce back
+        ball.bounce_y()
 
+    # Right paddle collision
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 and ball.x_move > 0:
+        ball.bounce_x()
+
+    # Left paddle collision
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -320 and ball.x_move < 0:
+        ball.bounce_x()
+
+    #detect if paddle miss the ball
+    #right paddle miss
+    if ball.xcor() >380 :
+        #reset ball to center
+        ball.center()
+        scoreboard.l_point()
+    #left paddle miss
+    if ball.xcor()<-380:
+        ball.center()
+        scoreboard.r_point()
+
+screen.exitonclick()
